2ad5676e0b660ad656ce59441540ec147808ec12
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-30 10:46:15 -0500

    login component finished

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,18 +1,53 @@
 <template>
-    <div>
-        Si sale esto es porque funcionó xd
+  <div>
+    <div class="background"></div>
+    <div class="form shadow-lg">
+      <h3>Iniciar sesión</h3>
+      <form>
+        <input class="form-control" placeholder="Usuario" />
+        <input type="password" class="form-control" placeholder="Password" />
+        <button class="btn btn-primary">Ingresar</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
-    export default{
-        name: "LogIn", // NOmbre del componente
-        data: function () {}, // Variables que se usan
-        methods: {}, // Métodos que estoy usando
-        created: function () {} // Eventos: Qué pasa cuando se inicia el componente
-    }
+export default {
+  name: "LogIn", // NOmbre del componente
+  data: function () {}, // Variables que se usan
+  methods: {}, // Métodos que estoy usando
+  created: function () {}, // Eventos: Qué pasa cuando se inicia el componente
+};
 </script>
     
 <style>
+.background {
+  background-image: url("https://images.pexels.com/photos/351264/pexels-photo-351264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+  height: calc(100vh - 57px);
+}
 
+.form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background-color: white;
+  transform: translate(-50%, -50%);
+  border-radius: 10px;
+  padding: 80px 50px;
+}
+
+.form form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form input {
+  margin-bottom: 25px;
+}
+
+.form h3{
+    text-align: center;
+    margin-bottom: 30px;
+}
 </style>
